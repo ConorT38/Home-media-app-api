cfcc803df1a205ce231e2fd396ed582e217564c6
Conor Thompson <conorthompson213@gmail.com>
2025-07-16 20:52:25 +0100

    adding names for shows automatically in torrents;

--- routes/torrents.ts
+++ routes/torrents.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { exec } from "child_process";
 import { mapToJsonArray } from "../utils/mapper.js";
+import { executeQuery } from "../utils/database.js";
+import { OkPacket } from "mysql2";
 
 const router = Router();
 
@@ -49,6 +51,7 @@ router.post("/download", async (req: Request, res: Response) => {
     try {
         const magnetLink = req.body.magnetUri as string;
         const category = req.body.category as string;
+        const name = req.query.name as string;
 
         if (!magnetLink) {
             res.status(400).json({ error: "Missing required body parameter: magnetLink" });
@@ -64,6 +67,24 @@ router.post("/download", async (req: Request, res: Response) => {
                 storageLocation = "/mnt/ext1/torrents/tv";
                 break;
         }
+
+        if (category.toLocaleLowerCase() === "shows") {
+            const insertShowSql = `
+            INSERT INTO shows (name, description, show_folder)
+            VALUES (?, ?, ?)
+            `;
+            const values = [name, name, storageLocation];
+
+            try {
+                const result = await executeQuery<OkPacket>(insertShowSql, values);
+                console.debug(`Inserted show with ID: ${result.insertId}`);
+            } catch (dbError) {
+                console.error(`Database error: ${dbError}`);
+                res.status(500).json({ error: "Failed to insert show into database" });
+                return;
+            }
+        }
+
         const command = `transmission-remote --auth pi:raspberry -a "${magnetLink}" -w "${storageLocation}"`;
         console.log(`Executing command: ${command}`);
         exec(command, (error, stdout, stderr) => {
